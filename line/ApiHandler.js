bb9403c7f9768f265572171d923125c7f49a58cf
twkhjl <twkhjl@gmail.com>
2023-10-09 17:53:15 +0800

    刪除無用api function;將原生fetch替換成node-fetch;

--- line/ApiHandler.js
+++ line/ApiHandler.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const fetch = require('node-fetch');
+
 const lineConfig = require("./config");
 const channelAccessToken = lineConfig.channelAccessToken;
 const channelSecret = lineConfig.channelSecret;
@@ -12,50 +14,23 @@ const ApiHandler = {
     curl -v -X GET https://api.line.me/v2/bot/group/{groupId}/summary \
     -H 'Authorization: Bearer {channel access token}'
     */
-    groupSummary: (groupId) => {
+    groupSummary: async (groupId) => {
         const url = api.groupSummary.replace("{groupId}", groupId);
         const accessToken = lineConfig.channelAccessToken;
 
-        return new Promise((resolve, reject) => {
-            const headers = {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${accessToken}`,
-            }
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+        }
 
-            fetch(url, {
-                headers,
-            })
-                .then((res) => resolve(res.json()))
-                .catch(function (err) {
-                    return reject(err);
-                })
-        });
+        const response = await fetch(url, {
+            headers: headers,
+        })
+        const data = await response.json();
 
-    },
+        return new Promise((resolve, reject) => resolve(data));
 
-    /**
-     curl -v -X GET https://api.line.me/v2/bot/group/{groupId}/members/ids?start={continuationToken} \
-    -H 'Authorization: Bearer {channel access token}'
-     
-     */
-    groupMembersIds: (groupId) => {
-        const url = api.groupMembersIds.replace("{groupId}", groupId);
-        const accessToken = lineConfig.channelAccessToken;
 
-        return new Promise((resolve, reject) => {
-            const headers = {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${accessToken}`,
-            }
-
-            fetch(url, {
-                headers,
-            })
-                .then((res) => resolve(res.json()))
-                .catch(function (err) {
-                    return reject(err);
-                })
-        });
 
     },
 
@@ -63,24 +38,22 @@ const ApiHandler = {
     curl -v -X GET https://api.line.me/v2/bot/profile/{userId} \
     -H 'Authorization: Bearer {channel access token}'
      */
-    userProfile: (userId) => {
+    userProfile: async (userId) => {
         const url = api.userProfile.replace("{userId}", userId);
         const accessToken = lineConfig.channelAccessToken;
 
-        return new Promise((resolve, reject) => {
-            const headers = {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${accessToken}`,
-            }
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+        }
 
-            fetch(url, {
-                headers,
-            })
-                .then((res) => resolve(res.json()))
-                .catch(function (err) {
-                    return reject();
-                })
-        });
+        const response = await fetch(url, {
+            headers: headers,
+        })
+        const data = await response.json();
+
+        return new Promise((resolve, reject) => resolve(data));
+
 
     }
 };
