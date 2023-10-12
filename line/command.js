c4ebf3f22ed0e90ed7d31cadfb55f439c7dd52ec
twkhjl <twkhjl@gmail.com>
2023-10-12 21:17:00 +0800

    修正講幹話模組下指令時無作用的問題

--- line/command.js
+++ line/command.js
@@ -1,10 +1,10 @@
 const command = {
     "readme": /^mic怎麼用$/,
 
-    "learnTrashTalk": /^mic學幹話 (\S+) (.+)$/gm,
-    "talkTrash": /^mic (.+)/gm,
-    "removeAllTrashTalk": /^mic幹話忘光光$/gm,
-    "removeOneTrashTalk": /^mic給我忘記 (.+)$/gm,
+    "learnTrashTalk": /^mic學幹話 (\S+) (.+)$/,
+    "talkTrash": /^mic (.+)$/,
+    "removeAllTrashTalk": /^mic幹話忘光光$/,
+    "removeOneTrashTalk": /^mic給我忘記 (.+)$/,
 
     "showRandomImg": /^mic圖呢$/,
 
