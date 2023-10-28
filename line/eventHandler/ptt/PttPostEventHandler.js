f9150911768ee665520b89cca9ebc1a3c7e19fc7
twkhjl <twkhjl@gmail.com>
2023-10-28 23:08:18 +0800

    修正ptt文章功能

--- line/eventHandler/ptt/PttPostEventHandler.js
+++ line/eventHandler/ptt/PttPostEventHandler.js
@@ -1,3 +1,4 @@
+const PttPostModel = require("../../../models/PttPostModel");
 const PttDataFilter = require("../../../utils/ptt/PttDataFilter");
 const PttDataHandler = require("../../../utils/ptt/PttDataHandler");
 const commandObj = require("../../command");
@@ -10,17 +11,15 @@ const PttPostEventHandler = async function (client, event) {
     // 顯示支援的板名
     if (commandObj.ptt.posts.showSupportedBoardName.regex.exec(event.message.text)) {
 
-        
-        
         const boardName = PttDataHandler.getSupportedBoardNameTW();
-        
+
         let outputMsg = "";
-        outputMsg+=`目前支援以下ptt板名的熱門文及噓文搜尋:\n`;
-        [].forEach.call(boardName,function(name){
-            outputMsg+=`${name}\n`;
+        outputMsg += `目前支援以下ptt板名的熱門文及噓文搜尋:\n`;
+        [].forEach.call(boardName, function (name) {
+            outputMsg += `${name}\n`;
         })
 
-        return lineReplyHandler.replyWithText(client,event,outputMsg);
+        return lineReplyHandler.replyWithText(client, event, outputMsg);
     }
 
     // 熱門文章
@@ -29,32 +28,30 @@ const PttPostEventHandler = async function (client, event) {
         const regex = new RegExp(commandObj.ptt.posts.showHotPosts.regex);
         const outputArr = regex.exec(event.message.text);
 
-        console.log(outputArr);
 
-        if(!outputArr[1]) return null;
+        if (!outputArr[1]) return null;
 
-        if(outputArr[1].length > 6) return null;
-        
-        const boardNameTW = outputArr[1].replace("板","");
-        if(!boardNameData[boardNameTW]) return null;
-        
+        if (outputArr[1].length > 6) return null;
+
+        const boardNameTW = outputArr[1].replace("板", "");
+        if (!boardNameData[boardNameTW]) return null;
+
         const boardName = boardNameData[boardNameTW];
-        
-        const pageTotal = await PttDataHandler.getPageTotal(boardName);
-        const minPageNum = (pageTotal*1 - 50)*1 <= 0 ? pageTotal : (pageTotal*1 - 50)*1;
-
-        const posts = await PttDataHandler.getPostsBetweenPageNums(boardName,minPageNum,pageTotal);
-        const filteredPosts = await PttDataFilter.filterDataWithPushCnt(posts,60);
-        const slicedPosts = filteredPosts.slice(0,10);
-
-        let outputMsg = "";
-        outputMsg+=`PTT${boardNameTW}板熱門文章(60推以上)\n`;
-        [].forEach.call(slicedPosts,function(post){
-            outputMsg+=`(${post.pushCnt})${post.name}\n`;
-            outputMsg+=`${post.link}\n\n`;
+        const pushCnt = 60;
+        const posts = await PttPostModel.selectByPushCntWithLimit({
+            boardName: boardName,
+            pushCnt: 60,
+            limit: 20,
         })
 
-        return lineReplyHandler.replyWithText(client,event,outputMsg);
+        let outputMsg = "";
+        outputMsg += `PTT${boardNameTW}板熱門文章(${pushCnt}推以上)\n`;
+        [].forEach.call(posts, function (post) {
+            outputMsg += `(${post.push_cnt})${post.title}\n`;
+            outputMsg += `${post.url}\n\n`;
+        })
+
+        return lineReplyHandler.replyWithText(client, event, outputMsg);
     }
 
     // 熱門噓文
@@ -63,35 +60,34 @@ const PttPostEventHandler = async function (client, event) {
         const regex = new RegExp(commandObj.ptt.posts.showBooPosts.regex);
         const outputArr = regex.exec(event.message.text);
 
-        if(!outputArr[1]) return null;
+        if (!outputArr[1]) return null;
 
-        if(outputArr[1].length > 3) return null;
-        
-        const boardNameTW = outputArr[1].replace("板","");
-        if(!boardNameData[boardNameTW]) return null;
-        
-        
-        
+        if (outputArr[1].length > 6) return null;
+
+        const boardNameTW = outputArr[1].replace("板", "");
+        if (!boardNameData[boardNameTW]) return null;
+
+
         const boardName = boardNameData[boardNameTW];
+        const pushCnt = 60;
+        const posts = await PttPostModel.selectByBooCntWithLimit({
+            boardName: boardName,
+            pushCnt: "X1",
+            limit: 20,
+        });
 
-        const pageTotal = await PttDataHandler.getPageTotal(boardName);
-        const minPageNum = (pageTotal*1 - 50)*1 <= 0 ? pageTotal : (pageTotal*1 - 50)*1;
-
-        const posts = await PttDataHandler.getPostsBetweenPageNums(boardName,minPageNum,pageTotal);
-        const filteredPosts = await PttDataFilter.filterDataWithBooCnt(posts,"X1");
-        const slicedPosts = filteredPosts.slice(0,10);
         let outputMsg = "";
-        outputMsg+=`PTT${boardNameTW}板熱門噓文(X1以上)\n`;
-        [].forEach.call(slicedPosts,function(post){
-            outputMsg+=`(${post.pushCnt})${post.name}\n`;
-            outputMsg+=`${post.link}\n\n`;
+        outputMsg += `PTT${boardNameTW}板熱門噓文(X1以上)\n`;
+        [].forEach.call(posts, function (post) {
+            outputMsg += `(${post.push_cnt})${post.title}\n`;
+            outputMsg += `${post.url}\n\n`;
         })
 
-        return lineReplyHandler.replyWithText(client,event,outputMsg);
+        return lineReplyHandler.replyWithText(client, event, outputMsg);
     }
 
 
-    
+
 
 
 
---
+++ models/PttPostModel.js
@@ -0,0 +1,105 @@
+const db = require("../database/DB");
+const mysql = require('mysql');
+
+const tableName = "ptt_posts";
+
+const PttPostModel = {
+
+    // 取得爆文
+    select100PushCntWithLimit: (data) => {
+        const boardName = data.boardName;
+        const limit = data.limit;
+        const sql = `
+            SELECT * FROM ${tableName}
+            WHERE board_name = ?
+            AND push_cnt = '爆' 
+            ORDER BY post_date DESC
+            LIMIT ${limit};
+        `;
+        const args = [boardName];
+        return new Promise((resolve, reject) => {
+            db.query(sql, args, (err, rows) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(rows);
+            });
+        });
+    },
+
+    // 取得熱門文章
+    selectByPushCntWithLimit: (data) => {
+        const boardName = data.boardName;
+        const limit = data.limit;
+        const pushCnt = data.pushCnt;
+        const sql = `
+            SELECT * FROM ${tableName}
+            WHERE board_name = ?
+            AND push_cnt >= ?
+            OR push_cnt = '爆' 
+            ORDER BY post_date DESC
+            LIMIT ${limit};
+        `;
+        const args = [boardName, pushCnt];
+        return new Promise((resolve, reject) => {
+            db.query(sql, args, (err, rows) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(rows);
+            });
+        });
+    },
+
+    // 取得噓文
+    selectByBooCntWithLimit: (data) => {
+        const boardName = data.boardName;
+        const limit = data.limit;
+        const pushCnt = data.pushCnt;
+        const sql = `
+            SELECT * FROM ${tableName}
+            WHERE board_name = ?
+            AND push_cnt  IN ( ? )
+            OR push_cnt = 'XX' 
+            ORDER BY post_date DESC
+            LIMIT ${limit};
+        `;
+        const args = [boardName, pushCnt];
+        return new Promise((resolve, reject) => {
+            db.query(sql, args, (err, rows) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(rows);
+            });
+        });
+    },
+
+    // 取得XX噓文
+    select100BooCntWithLimit: (data) => {
+        const boardName = data.boardName;
+        const limit = data.limit;
+        const pushCnt = data.pushCnt;
+        const sql = `
+            SELECT * FROM ${tableName}
+            WHERE board_name = ?
+            AND push_cnt = 'XX' 
+            ORDER BY post_date DESC
+            LIMIT ${limit};
+        `;
+        const args = [boardName, pushCnt];
+        return new Promise((resolve, reject) => {
+            db.query(sql, args, (err, rows) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(rows);
+            });
+        });
+    },
+
+
+
+}
+
+module.exports = PttPostModel
